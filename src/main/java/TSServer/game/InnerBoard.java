4d255a988d26e6bbf90286e2291af323a30e7be7
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-10-25 14:35:38 -0400

    :sparkle: Finish initial inner board state

--- src/main/java/TSServer/game/InnerBoard.java
+++ src/main/java/TSServer/game/InnerBoard.java
@@ -10,6 +10,9 @@ public class InnerBoard {
         // First two columns (0, 1)
         fillSlots(0, 2, 0, 11, Slot.FORBIDDEN_SPACE);
 
+        // Last two columns (9,10)
+        fillSlots(9, 11, 0, 11, Slot.FORBIDDEN_SPACE);
+
         // Column 2
         fillSlots(2,3, 0, 3, Slot.FORBIDDEN_SPACE);
         fillSlots(2, 3, 3, 7, Slot.NOT_OCCUPIED);
@@ -20,17 +23,35 @@ public class InnerBoard {
         fillSlots(3, 4, 3, 8, Slot.NOT_OCCUPIED);
         fillSlots(3, 4, 8, 11, Slot.FORBIDDEN_SPACE);
 
-        // Rows 4, 5, 6
-        fillSlots(4, 7, 0, 1, Slot.FORBIDDEN_SPACE);
+        // Column 4-5-6
+        for (int i = 0; i <= 2; i++ ) {
+            fillSlots(4+i, 5+i, 0, 2, Slot.FORBIDDEN_SPACE);
+            fillSlots(4+i, 5+i, 2, 9, Slot.NOT_OCCUPIED);
+            fillSlots(4+i, 5+i, 9, 11, Slot.FORBIDDEN_SPACE);
+        }
+
+        // Column 7
+        fillSlots(7,8, 0, 3, Slot.FORBIDDEN_SPACE);
+        fillSlots(7, 8, 3, 8, Slot.NOT_OCCUPIED);
+        fillSlots(7, 8, 8, 11, Slot.FORBIDDEN_SPACE);
+
+        // Column 8
+        fillSlots(8,9, 0, 3, Slot.FORBIDDEN_SPACE);
+        fillSlots(8, 9, 3, 7, Slot.NOT_OCCUPIED);
+        fillSlots(8, 9, 7, 11, Slot.FORBIDDEN_SPACE);
+
+        // Middle empty forbidden
+        boardArray[5][5] = Slot.FORBIDDEN_SPACE;
+
     }
 
     /**
      * Fills slots of the boardArray with
      * a slot value from a given start and end position
-     * @param xStart
-     * @param xEnd
-     * @param yStart
-     * @param yEnd
+     * @param xStart (inclusive)
+     * @param xEnd (non-inclusive)
+     * @param yStart (inclusive)
+     * @param yEnd (non-inclusive)
      * @param value
      */
     private void fillSlots(int xStart, int xEnd, int yStart, int yEnd, Slot value) {
