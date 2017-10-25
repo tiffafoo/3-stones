cd7ba111e6e1b2a39c82ea0ca6e058b03547900d
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-10-25 14:10:10 -0400

    :sparkles: Add packet

--- src/main/java/TSServer/Packet.java
+++ src/main/java/TSServer/Packet.java
@@ -1,6 +1,69 @@
 package TSServer;
 
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * Handles reading and writing packets
+ */
 public class Packet {
-    // TODO: sendPacket()
-    // TODO: receivePacket()
+    private final org.slf4j.Logger log = LoggerFactory.getLogger(this.getClass().getName());
+    // TODO: Replace 32 with actual bufsize
+    public static final int BUFSIZE = 32;
+
+    /**
+     * Reads bytes from stream
+     *
+     * @param socket
+     *          Socket that receives data
+     * @return bytes read from the stream
+     * @throws IOException
+     */
+    public byte[] read(Socket socket) throws IOException {
+        InputStream in = socket.getInputStream();
+        byte[] buffer = new byte[BUFSIZE];
+
+        int totalBytesRcvd = 0;
+        int bytesRcvd = 0;
+
+        // Continue reading until we have received
+        // the size expected
+        while (totalBytesRcvd < BUFSIZE) {
+            if ((bytesRcvd = in.read(buffer, totalBytesRcvd, BUFSIZE - totalBytesRcvd)) == -1) {
+                throw new IOException("Connection closed due to failure");
+            }
+            totalBytesRcvd += bytesRcvd;
+        }
+
+        log.debug("Received packet: " + buffer);
+
+        return buffer;
+    }
+
+    /**
+     * Write bytes to stream
+     *
+     * @param bytes
+     *          Bytes to write
+     * @param socket
+     *          Socket that will be written to
+     * @throws IOException
+     */
+    public void write(byte[] bytes, Socket socket) throws IOException {
+        // Check size is what we expect
+        if (bytes.length != BUFSIZE) {
+            throw new IOException("Packet size is: " + bytes.length
+                    + " instead of [" + BUFSIZE +"]" );
+        }
+
+        log.debug("Writing packet: " + bytes);
+
+        // Write bytes
+        OutputStream out = socket.getOutputStream();
+        out.write(bytes);
+    }
 }
